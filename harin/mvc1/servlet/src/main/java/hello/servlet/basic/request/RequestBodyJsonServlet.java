3d04ed32aaf8ad10d5c3ff1819360e2854289328
hahaharin <harin2950@naver.com>
2023-12-18 16:44:56 +0900

    [mvc1-2] HTTP 요청 데이터 - API 메시지 바디 - JSON

--- harin/mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ harin/mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
@@ -13,10 +15,18 @@ import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "RequestBodyJsonServlet", urlPatterns = "/request-body-json")
 public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
         System.out.println("messageBody = " + messageBody);
+
+        // 객체로 변환
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData.getUsername() = " + helloData.getUsername());
+        System.out.println("helloData.getAge() = " + helloData.getAge());
+        response.getWriter().write("ok");
     }
 }
