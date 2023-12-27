7134d1c9349296839e25db096ebf121be0799612
hahaharin <harin2950@naver.com>
2023-12-27 18:01:35 +0900

    [mvc1-4] View 분리 - v2

---
+++ harin/mvc1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,51 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*") //url 매핑 중요
+public class FrontControllerServletV2 extends HttpServlet {
+
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+
+    //생성자
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV2.service");
+
+        //<로직>
+        String requestURI = request.getRequestURI(); //front-controller/v1/members/new-form 를 얻어낼 수 있음
+
+        // 다형성에 의해 부모인 ControllerV1으로부터 받음
+        ControllerV2 controller = controllerMap.get(requestURI); //map에서 uri 꺼내면 controllerV1이 찾아짐. MemberFormControllerV1 객체 인스턴스가 반환
+
+        if (controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyView view = controller.process(request, response);
+        view.render(request,response);
+    }
+}
