10d16d8a8898ca143fd2507e68a9822263a9de32
2522001 <minseo770@gmail.com>
2023-12-28 21:27:00 +0900

    [mvc1-6] HTTP 요청 파라미터 - @ModelAttribute

--- minseo/mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ minseo/mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -105,6 +107,31 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    /**
+     *@ModelAttribute 사용:
+     * HelloData 객체 생성 -> 요청 파라미터의 이름으로 HelloData 객체의 프로퍼티를 찾는다.
+     * -> 해당 프로퍼티의 setter를 호출해서 파라미터의 값을 바인딩한다.
+     * ex) username 파라미터 -> setUsername() 메서드를 찾아 호출하면서 값을 입력한다.
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 가능
+     * String, int, Integer 같은 단순 타입 -> @RequestParam
+     * argument resolver로 지정해둔 타입 외 -> @ModelAttribute
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
