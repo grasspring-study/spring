642fbada5152b2873b6f908fd82de69f040b06be
2522001 <minseo770@gmail.com>
2023-12-28 18:31:48 +0900

    [mvc1-6]  HTTP 요청 파라미터 - @RequestParam

--- minseo/mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ minseo/mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -5,14 +5,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
 public class RequestParamController {
 
-    // 반환 타입이 없으면서 아래와 같이 응답에 값을 직접 집어 넣을 경우 view를 조회하지 않는다.
+    /**
+     * 반환 타입이 없으면서 아래와 같이 응답에 값을 직접 집어 넣을 경우 view를 조회하지 않는다.
+     */
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -20,5 +25,86 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    /**
+     * @RequestParam 사용
+     * @ResponseBody 추가:
+     * 어노테이션이 @RestController가 아닌 @Controller이므로 return값에 해당하는 view를 조회하는데,
+     * @ResponseBody를 추가하여 view 조회를 무시하고 HTTP message body에 해당 내용을 넣는다.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * String, int 등의 단순 타입이면 @RequestParam도 생략 가능
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String reqeustParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam.required
+     * /request-param 필수 파라미터가 빠진 경우: 400
+     * /request-param?username= 파라미터 이름만 있고 값이 없는 경우 -> 빈 문자로 통과
+     * int 대신 Integer(객체)를 사용해야 함. -> 기본형(primitive)에는 null을 입력할 수 없다.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam defaultValue 사용
+     * 빈 문자의 경우에도 defaultValue가 적용된다.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) { // null이 들어갈 일이 없으므로 기본형인 int 사용 가능
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam Map, MultiValueMap
+     * Map(key=value)
+     * MultiValueMap(key=[value1, value2, ...]) ex) (key=userIds, value=[id1, id2])
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
     }
 }
