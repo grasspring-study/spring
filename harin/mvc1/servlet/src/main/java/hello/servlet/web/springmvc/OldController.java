27a49f4174aa17dbdb8cb7dbf1474c226a71377d
hahaharin <harin2950@naver.com>
2024-01-01 23:33:04 +0900

    [mvc1-5] 뷰 리졸버

--- harin/mvc1/servlet/src/main/java/hello/servlet/web/springmvc/OldController.java
+++ harin/mvc1/servlet/src/main/java/hello/servlet/web/springmvc/OldController.java
@@ -11,6 +11,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView(("new-form")); //논리명만 넣음. 뷰 리졸버에서 물리명으로 바꿔줘야함
     }
 }
