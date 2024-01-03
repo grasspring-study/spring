6f5600ae66f1cdc55f65bc364764f77567f9ba8c
2522001 <minseo770@gmail.com>
2024-01-03 23:35:02 +0900

    [mvc2-2] 체크 박스 - 멀티

--- minseo/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ minseo/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,21 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * 각각의 컨트롤러에서 model.addAttribute(...)를 사용해서 체크 박스를 구성하는 데이터를 반복해서 넣어주어야 한다.
+     * @ModelAttribute는 이렇게 컨트롤러에 있는 별도의 메서드에 적용할 수 있다.
+     * 해당 컨트롤러를 요청할 때 regions에서 반환한 값이 자동으로 model에 담기게 된다.
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -44,6 +61,8 @@ public class FormItemController {
         log.info("item.open={}", item.getOpen());
         // HTML에서 체크 박스를 선택하지 않고 폼을 전송하면 open이라는 필드 자체가 서버로 전송되지 않는다. (false가 아니라 null이 된다.)
         // -> HTML에 히든 필드를 만들어서 해당 문제를 해결한다. 기존 체크 박스 이름 앞에 _를 붙여서 전송하면 체크를 해제했다고 인식할 수 있다.
+
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
