73bbda2e3ea69808ec94a857d6e75ab5498d1a28
hahaharin <harin2950@naver.com>
2024-01-07 18:58:49 +0900

    [mvc2-1] 템플릿 조각

--- harin/mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ harin/mvc2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -124,6 +124,7 @@ public class BasicController {
         return "basic/javascript";
     }
 
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
