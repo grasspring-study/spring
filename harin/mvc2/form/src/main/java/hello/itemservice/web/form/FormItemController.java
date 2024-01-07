985f96400ea0318fa0dc954e4ad87e52e1f10f96
hahaharin <harin2950@naver.com>
2024-01-07 21:53:13 +0900

    [mvc2-2] 셀렉트 박스

--- harin/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ harin/mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +25,7 @@ public class FormItemController {
     private final ItemRepository itemRepository;
 
     // 모델에 항상 담겨있음
+    // map
     @ModelAttribute("regions")
     public Map<String, String> regions(){
         Map<String, String> regions = new LinkedHashMap<>(); // 순서 보장
@@ -32,9 +35,20 @@ public class FormItemController {
         return regions;
     }
 
+    // enum
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes(){
         return ItemType.values();
+    }
+
+    // list
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
     }
 
     @GetMapping
