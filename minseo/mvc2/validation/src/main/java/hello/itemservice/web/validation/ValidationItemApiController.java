f4fdfd2fa0b15030f331dee1deb7ad0c3c1eecae
2522001 <minseo770@gmail.com>
2024-01-09 16:07:16 +0900

    [mvc2-5] Bean Validation - HTTP 메시지 컨버터

---
+++ minseo/mvc2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,37 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    /**
+     * @RequestBody:
+     * HttpMessageConverter는 전체 객체 단위로 적용되며, 메시지 컨버터의 작동이 성공해서 ItemSaveForm 객체를 만들어야 Validator가 적용된다.
+     *
+     * @ModelAttribute:
+     * 필드 단위로 바인딩이 적용되므로 특정 필드가 바인딩 되지 않아도 나머지 필드는 정상 바인딩 되고, Validator를 사용한 검증도 적용할 수 있다.
+     */
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+
+        log.info("API 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생 errors={}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("성공 로직 실행");
+        return form;
+    }
+}
