3eb31f0c7d54b17a3fff761026d084c330d0b317
2522001 <minseo770@gmail.com>
2024-01-16 14:45:08 +0900

    [mvc2-8] 서블릿 예외 처리 - 필터

---
+++ minseo/mvc2/exception/src/main/java/hello/exception/WebConfig.java
@@ -0,0 +1,26 @@
+package hello.exception;
+
+import hello.exception.filter.LogFilter;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
+        // 이렇게 두 가지를 모두 넣으면 클라이언트 요청은 물론이고, 오류 페이지 요청에서도 필터가 호출된다.
+        // 아무것도 넣지 않으면 기본 값이 `DispatcherType.REQUEST` 이다.
+        return filterRegistrationBean;
+
+    }
+}
