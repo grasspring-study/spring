b3483b80956312e9c81447d9f21cc5f0623c5b7b
2522001 <minseo770@gmail.com>
2024-01-16 15:32:46 +0900

    [mvc2-8] 서블릿 예외 처리 - 인터셉터

--- minseo/mvc2/exception/src/main/java/hello/exception/WebConfig.java
+++ minseo/mvc2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,17 +1,19 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -22,5 +24,13 @@ public class WebConfig implements WebMvcConfigurer {
         // 아무것도 넣지 않으면 기본 값이 `DispatcherType.REQUEST` 이다.
         return filterRegistrationBean;
 
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
     }
 }
---
+++ minseo/mvc2/exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -0,0 +1,45 @@
+package hello.exception.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+/**
+ * 앞서 필터의 경우에는 필터를 등록할 때 어떤 `DispatcherType` 인 경우에 필터를 적용할 지 선택할 수 있었다. 그런 데 인터셉터는 서블릿이 제공하는 기능이 아니라 스프링이 제공하는 기능이다.
+ * 따라서 `DispatcherType` 과 무관하게 항상 호출된다.
+ * 대신에 인터셉터는 다음과 같이 요청 경로에 따라서 추가하거나 제외하기 쉽게 되어 있기 때문에, 이러한 설정을 사용해 서 오류 페이지 경로를 `excludePathPatterns` 를 사용해서 빼주면 된다.
+ */
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    public static final String LOG_ID = "logId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        request.setAttribute(LOG_ID, uuid);
+        log.info("REQUEST  [{}][{}][{}][{}]", uuid, request.getDispatcherType(),
+                requestURI, handler);
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String)request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(),
+                requestURI);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
+    }
+}
